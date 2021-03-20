de166b9c57090520fe7aec03ccc6e929368c2031
Munir <info.mubir@gmail.com>
2021-03-20 21:01:40 +0900

    dining philosoper init

---
+++ dining_philosopher_imp/Constants.java
@@ -0,0 +1,12 @@
+public class Constants {
+    // private sothat we cant isntantiate it from other class
+    // as it needed to be authentic
+    private Constants()
+    {
+
+    }
+
+    public static final int COUNT_PHILOSOPHER =5;
+    public static final int COUNT_CHOPSTICK= 5;
+    public static final int RUNNING_TIME =5*1000;
+}
